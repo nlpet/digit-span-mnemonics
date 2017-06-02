1237673e3f60d590a81012c46388db93652d5d80
Nora Petrova <schottkey@gmail.com>
2017-06-02 09:52:38 +0100

    refactor: Remove unneeded functions

--- src/utils.js
+++ src/utils.js
@@ -1,14 +1,3 @@
-import { List as list } from 'immutable';
-
-const initializeList = (n, val) => {
-    return list(Array(n).fill(val));
-};
-
-const matrix = (n, m, val) => {
-    return initializeList(n).map(() => initializeList(m, val));
-};
 
 export {
-    matrix,
-    initializeList
 };

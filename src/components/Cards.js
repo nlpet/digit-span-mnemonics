d7bbb7ee0face9f748ca859fcee418b1f7a61b78
Nora Petrova <nora.axion@gmail.com>
2017-08-03 17:32:28 +0100

    Remove card #10

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -4,19 +4,6 @@ import { Card, Divider } from 'semantic-ui-react';
 const Cards = () => {
     return (
         <Card.Group>
-           <Card>
-                <Card.Content>
-                    <Card.Header><h1>9</h1></Card.Header>
-                    <Card.Meta>/p/, /b/</Card.Meta>
-                    <Card.Meta>
-                        <b>hint:</b> p looks like 9 flipped horizontally,
-                                     b looks like the 9 turned 180°
-                    </Card.Meta>
-                    <Divider />
-                    <Card.Content>p, b, gh (in hiccough)</Card.Content>
-                </Card.Content>
-            </Card>
-
         </Card.Group>
     );
 };

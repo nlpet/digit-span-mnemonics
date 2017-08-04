6e7405768ecf3bbc977286b0f1de7e1bb1e0b535
Nora Petrova <schottkey@gmail.com>
2017-08-04 10:18:57 +0100

    Mess up more reducers #3

--- src/reducers/navigation.js
+++ src/reducers/navigation.js
@@ -2,13 +2,4 @@ import { merge } from 'ramda';
 
 import { CHANGE_MODE } from '../constants/actionTypes';
 
-const navigation = (state = {}, action) => {
-    switch (action.type) {
-        case CHANGE_MODE:
-            return merge(state, { mode: action.payload.mode });
-        default:
-            return state;
-    }
-};
-
 export default navigation;

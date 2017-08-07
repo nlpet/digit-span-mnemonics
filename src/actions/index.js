8396794a272baa51dad1e4217ec2e6c5054bdc4c
Nora Petrova <schottkey@gmail.com>
2017-08-07 11:03:45 +0100

    remove action at 11:03

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,10 +1,5 @@
 import * as actionTypes from '../constants/actionTypes';
 
-export const startChallenge = ({ timer }) => ({
-    type: actionTypes.START_CHALLENGE,
-    payload: { timer }
-});
-
 export const markPracticeAnswer = ({ correct, answer, correctAnswer }) => ({
     type: actionTypes.MARK_PRACTICE_ANSWER,
     payload: { correct, answer, correctAnswer }

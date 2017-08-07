de57ee4762dd9c89f57143c707322a43681eabfc
Nora Petrova <schottkey@gmail.com>
2017-08-07 10:54:09 +0100

    remove action at 10:54

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,10 +1,5 @@
 import * as actionTypes from '../constants/actionTypes';
 
-export const changeMode = ({ mode }) => ({
-    type: actionTypes.CHANGE_MODE,
-    payload: { mode }
-});
-
 export const startPractice = () => ({
     type: actionTypes.START_PRACTICE,
     payload: { }

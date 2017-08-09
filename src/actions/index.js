469f4cf937b9ca17c4e0b4999ef5491fd377655b
Nora Petrova <schottkey@gmail.com>
2017-08-09 14:29:02 +0100

    removing one of the last actions remaining at 14:29

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,11 +1,6 @@
 import * as actionTypes from '../constants/actionTypes';
 
 
-export const generateNumber = () => ({
-    type: actionTypes.GENERATE_NEW_NUMBER,
-    payload: { }
-});
-
 export const setTimerToZero = () => ({
     type: actionTypes.SET_TIMER_TO_ZERO,
     payload: { }

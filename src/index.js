d19730695729d2998dc786b0cd4d3889ec2d8744
Nora Petrova <schottkey@gmail.com>
2017-08-14 11:27:14 +0100

    remove at 11:27

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-import { createStore } from 'redux';
 // import { createStore, compose } from 'redux';
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
